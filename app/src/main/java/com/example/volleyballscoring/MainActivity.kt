7df741541693fc31069b232b6344b6bd003b8381
luke withey <withey89@gmail.com>
2020-08-20 13:25:37 +0100

    Edit alignment and arrangement

--- app/src/main/java/com/example/volleyballscoring/MainActivity.kt
+++ app/src/main/java/com/example/volleyballscoring/MainActivity.kt
@@ -2,25 +2,24 @@ package com.example.volleyballscoring
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.Composable
-import androidx.compose.remember
+import androidx.compose.*
 import androidx.ui.core.*
 import androidx.ui.foundation.Icon
 import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.input.KeyboardType
 import androidx.ui.layout.*
 import androidx.ui.material.*
 import androidx.ui.res.imageResource
 import androidx.ui.res.vectorResource
 import androidx.ui.text.style.TextAlign
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.TextUnit
 import androidx.ui.unit.dp
 import com.example.volleyballscoring.ui.VolleyballScoringTheme
 import com.example.volleyballscoring.ui.typography
-import com.google.android.material.tabs.TabLayout
 
 private const val TAG = "MainActivity"
 
@@ -31,6 +30,7 @@ class MainActivity : AppCompatActivity() {
             VolleyballScoringTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
+
                     HomeScreen()
                 }
             }
@@ -39,47 +39,14 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun NewsStory() {
-
-    val image = imageResource(id = R.drawable.header)
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-
-        val imageModifier = Modifier
-            .preferredHeightIn(maxHeight = 180.dp)
-            .fillMaxWidth()
-            .clip(shape = RoundedCornerShape(16.dp)) // clip not showing on preview
-
-        Image(image, modifier = imageModifier, contentScale = ContentScale.Crop)
-
-        Spacer(Modifier.preferredHeight(16.dp))
-
-        Text(
-            "A party at a neighbours",
-            style = typography.h6
-        )
-        Text(
-            "Brighton, UK",
-            style = typography.body2
-        )
-        Text(
-            "August, 2020",
-            style = typography.body2
-        )
-
-        Spacer(modifier = Modifier.preferredHeight(16.dp))
-
-
-    }
-
-
-}
-
-@Composable
 fun HomeScreen(
     scaffoldState: ScaffoldState = remember { ScaffoldState() }
 ) {
+
+    val vectorModifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+
 
     Scaffold(scaffoldState = scaffoldState,
         topAppBar = {
@@ -87,28 +54,21 @@ fun HomeScreen(
                 Text(text = "Beach Volleyball Score Keeper", textAlign = TextAlign.Start)
             },
                 navigationIcon = {
-                    Icon(vectorResource(id = (R.drawable.ic_sports_volleyball_24px)))
+                    Icon(vectorResource(id = (R.drawable.ic_sports_volleyball_24px)), modifier = vectorModifier)
+
                 }
             )
         },
         bodyContent = {
-            val modifier = Modifier.padding(16.dp)
             HomeScreenBody()
         }
     )
-
-//    Column(modifier = Modifier.padding(16.dp)) {
-//        Text(text = "Beach Volleyball Score Card",
-//        style = typography.h1,
-//        textAlign = TextAlign.Center)
-//    }
-//
-//    Button(modifier = Modifier)
 }
 
 @Composable
-private fun HomeScreenBody(modifier: Modifier = Modifier) {
-    Column(modifier = Modifier.padding(16.dp)) {
+private fun HomeScreenBody() {
+    Column(modifier = Modifier.padding(16.dp)
+        ) {
         // add some composables
         HomeScreenCurrentScore()
 
@@ -118,26 +78,50 @@ private fun HomeScreenBody(modifier: Modifier = Modifier) {
     }
 }
 
+
 @Composable
 private fun HomeScreenCurrentScore() {
-    Row() {
-        AddRemoveButton()
-        AddRemoveButton()
+    Row(modifier = Modifier.fillMaxWidth(),
+    horizontalArrangement = Arrangement.SpaceEvenly,
+    verticalGravity = Alignment.CenterVertically) {
+        val scoreTeamOne = state {0}
+        val scoreTeamTwo = state {0}
+
+        AddRemoveButton(count = scoreTeamOne)
+        Column(verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalGravity = Alignment.CenterHorizontally) {
+
+            Text(
+                text = "Team One: Team Two"
+            )
+            Text(
+                text = "${scoreTeamOne.value}:${scoreTeamTwo.value}",
+                style = typography.h1,
+                fontSize = TextUnit.Companion.Sp(40)
+            )
+        }
+
+        AddRemoveButton(count = scoreTeamTwo)
     }
 }
 
 @Composable
-private fun AddRemoveButton() {
+private fun AddRemoveButton(count: MutableState<Int>) {
     Column() {
-        IconButton(onClick = { Log.d(TAG, "AddRemoveButton: Up Button Clicked") }) {
+        IconButton(onClick = {
+            count.value++
+            Log.d(TAG, "AddRemoveButton: Up Button Clicked count = ${count.value}")
+        }) {
             Icon(asset = vectorResource(id = R.drawable.ic_arrow_circle_up_24px))
         }
-        IconButton(onClick = { Log.d(TAG, "AddRemoveButton: Down Button Clicked") }) {
+        IconButton(onClick = {
+            Log.d(TAG, "AddRemoveButton: Down Button Clicked count = ${count.value}")
+            count.value--
+        }) {
             Icon(asset = vectorResource(id = R.drawable.ic_arrow_circle_down_24px))
         }
     }
 }
-
 
 
 @Preview
